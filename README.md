751327b78df8eec8215e6c72a2007573a1fda2e0
xuejiazhi <xuejiazhi@jtexpress.com>
2022-11-29 18:36:22 +0800

    feat: Java学习

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Java-Learn
 
 > Java学习笔记，为考研而战！
+
+学习资料主要来自于：
+
+- Java学习
+- [Java全栈知识体系](https://www.pdai.tech/)
--- day01/Demo.java
+++ day01/Demo.java
@@ -3,10 +3,21 @@ package day01;
 public class Demo {
 
   public static void main(String[] args) {
-    Integer x = 1;
-    int y = x;
+    // Integer x = 1;
+    // int y = x;
 
-    System.out.println("x --> " + x);
-    System.out.println("y --> " + y);
+    // System.out.println("x --> " + x);
+    // System.out.println("y --> " + y);
+
+    Integer x = new Integer(111);
+
+    Integer y = new Integer(123);
+
+    System.out.println(x == y); // false
+
+    Integer z = Integer.valueOf(123);
+    Integer k = Integer.valueOf(123);
+
+    System.out.println(z == k); // true
   }
 }
